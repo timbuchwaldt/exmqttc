5f00a4f7056ebe6199058cc75c0f3d10a027051d
Tim Buchwaldt <tim.buchwaldt@grandcentrix.net>
2017-06-03 23:45:15 +0200

    Imcrease assert_receive timeouts

--- test/exmqttc_test.exs
+++ test/exmqttc_test.exs
@@ -9,39 +9,39 @@ defmodule ExmqttcTest do
 
   test "connecting with minimal options" do
     {:ok, pid} = Exmqttc.start_link(Exmqtt.Testclient, [], host: '127.0.0.1')
-    assert_receive :connected
+    assert_receive :connected, 250
     Exmqttc.disconnect(pid)
   end
 
   test "connecting with registered names" do
     {:ok, pid} = Exmqttc.start_link(Exmqtt.Testclient, [name: :my_client], host: '127.0.0.1')
-    assert_receive :connected
+    assert_receive :connected, 250
     Exmqttc.disconnect(pid)
   end
 
   test "connecting with enhanced options" do
     {:ok, pid} = Exmqttc.start_link(Exmqtt.Testclient, [name: :my_client_2], keepalive: 30, host: '127.0.0.1')
-    assert_receive :connected
+    assert_receive :connected, 250
     Exmqttc.disconnect(pid)
   end
 
   test "subscribing and sending" do
     {:ok, pid} = Exmqttc.start_link(Exmqtt.Testclient, [name: :my_client_3], keepalive: 30, host: '127.0.0.1')
-    assert_receive :connected
+    assert_receive :connected, 250
 
     Exmqttc.subscribe(:my_client_3, "test")
     Exmqttc.publish(:my_client_3, "test", "foo")
-    assert_receive {:publish, "test", "foo"}
+    assert_receive {:publish, "test", "foo"}, 250
     Exmqttc.disconnect(pid)
   end
 
   test "synchronous subscribing and sending" do
     {:ok, pid} = Exmqttc.start_link(Exmqtt.Testclient, [name: :my_client_4], keepalive: 30, host: '127.0.0.1')
-    assert_receive :connected
+    assert_receive :connected, 250
 
     Exmqttc.sync_subscribe(:my_client_4, "test2")
     Exmqttc.sync_publish(:my_client_4, "test2", "foo")
-    assert_receive {:publish, "test2", "foo"}
+    assert_receive {:publish, "test2", "foo"}, 250
     Exmqttc.disconnect(pid)
   end
 
