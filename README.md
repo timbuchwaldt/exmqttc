53d37f466e29e35815ae40d8c11069c3c7b35ef7
Tim Buchwaldt <tim@buchwaldt.ws>
2017-06-16 10:51:50 +0200

    Improve readme, finish renaming to exmqttc

--- README.md
+++ README.md
@@ -5,12 +5,49 @@ Elixir wrapper for the emqttc library.
 emqttc must currently be installed manually as it is not available on hex (yet).
 ## Installation
 
-The package can be installedby adding `exmqttc` and `emqttc` to your list of dependencies in `mix.exs`:
+The package can be installed by adding `exmqttc` and `emqttc` to your list of dependencies in `mix.exs`:
 
 ```elixir
 def deps do
-  [{:exmqttc, "~> 0.1.0"}, {:emqttc, github: "emqtt/emqttc"}]
+  [{:exmqttc, "~> 0.3.0"}, {:emqttc, github: "emqtt/emqttc"}]
 end
 ```
 
-The docs can be found at [https://hexdocs.pm/exmqttc](https://hexdocs.pm/exmqttc).
+
+# Usage
+
+Create a callback module:
+```elixir
+defmodule MyClient do
+  require Logger
+
+  def init do
+    {:ok, []}
+  end
+
+  def handle_connect(state) do
+    Logger.debug "Connected"
+    {:ok, state}
+  end
+
+  def handle_disconnect(state) do
+    Logger.debug "Disconnected"
+    {:ok, state}
+  end
+
+  def handle_publish(topic, payload, state) do
+    Logger.debug "Message received on topic #{topic} with payload #{payload}"
+    {:ok, state}
+  end
+end
+```
+
+You can keep your own state and return it just like with `:gen_server`.
+
+Start the MQTT connection process by calling `start_link/3`:
+```elixir
+{:ok, pid} = Exmqttc.start_link(MyClient, [], host: '127.0.0.1')
+```
+
+
+Further docs can be found at [https://hexdocs.pm/exmqttc](https://hexdocs.pm/exmqttc).
--- test/exmqttc_test.exs
+++ test/exmqttc_test.exs
@@ -8,25 +8,25 @@ defmodule ExmqttcTest do
   end
 
   test "connecting with minimal options" do
-    {:ok, pid} = Exmqttc.start_link(Exmqtt.Testclient, [], host: '127.0.0.1')
+    {:ok, pid} = Exmqttc.start_link(Exmqttc.Testclient, [], host: '127.0.0.1')
     assert_receive :connected, 250
     Exmqttc.disconnect(pid)
   end
 
   test "connecting with registered names" do
-    {:ok, pid} = Exmqttc.start_link(Exmqtt.Testclient, [name: :my_client], host: '127.0.0.1')
+    {:ok, pid} = Exmqttc.start_link(Exmqttc.Testclient, [name: :my_client], host: '127.0.0.1')
     assert_receive :connected, 250
     Exmqttc.disconnect(pid)
   end
 
   test "connecting with enhanced options" do
-    {:ok, pid} = Exmqttc.start_link(Exmqtt.Testclient, [name: :my_client_2], keepalive: 30, host: '127.0.0.1')
+    {:ok, pid} = Exmqttc.start_link(Exmqttc.Testclient, [name: :my_client_2], keepalive: 30, host: '127.0.0.1')
     assert_receive :connected, 250
     Exmqttc.disconnect(pid)
   end
 
   test "subscribing and sending" do
-    {:ok, pid} = Exmqttc.start_link(Exmqtt.Testclient, [name: :my_client_3], keepalive: 30, host: '127.0.0.1')
+    {:ok, pid} = Exmqttc.start_link(Exmqttc.Testclient, [name: :my_client_3], keepalive: 30, host: '127.0.0.1')
     assert_receive :connected, 250
 
     Exmqttc.subscribe(:my_client_3, "test")
@@ -36,7 +36,7 @@ defmodule ExmqttcTest do
   end
 
   test "synchronous subscribing and sending" do
-    {:ok, pid} = Exmqttc.start_link(Exmqtt.Testclient, [name: :my_client_4], keepalive: 30, host: '127.0.0.1')
+    {:ok, pid} = Exmqttc.start_link(Exmqttc.Testclient, [name: :my_client_4], keepalive: 30, host: '127.0.0.1')
     assert_receive :connected, 250
 
     Exmqttc.sync_subscribe(:my_client_4, "test2")
