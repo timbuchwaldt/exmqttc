183443fce8c7b9a969a791f109930fee4330c7a7
Tim Buchwaldt <tim.buchwaldt@grandcentrix.net>
2018-03-18 16:10:21 +0100

    Release 0.5.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Exmqttc.Mixfile do
   def project do
     [
       app: :exmqttc,
-      version: "0.4.3",
+      version: "0.5.0",
       elixir: "~> 1.4",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
