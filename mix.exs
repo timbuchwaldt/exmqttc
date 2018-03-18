f73f4ba73138db79bab833d7d3de794bbb8df24b
Tim Buchwaldt <tim.buchwaldt@grandcentrix.net>
2018-03-18 13:19:02 +0100

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -2,15 +2,16 @@ defmodule Exmqttc.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :exmqttc,
-     version: "0.4.1",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps(),
-     package: package(),
-     docs: [main: "readme.html#usage", extras: ["README.md"]]
-   ]
+    [
+      app: :exmqttc,
+      version: "0.4.1",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      package: package(),
+      docs: [main: "readme.html#usage", extras: ["README.md"]]
+    ]
   end
 
   def application do
@@ -22,9 +23,10 @@ defmodule Exmqttc.Mixfile do
       name: :exmqttc,
       files: ["lib", "mix.exs", "README*", "LICENSE*"],
       maintainers: ["Tim Buchwaldt"],
-      description: "Elixir wrapper for the emqttc library. Some of the features: Reconnection, offline queueing, gen_* like callback APIs, QoS 0-2.",
+      description:
+        "Elixir wrapper for the emqttc library. Some of the features: Reconnection, offline queueing, gen_* like callback APIs, QoS 0-2.",
       licenses: ["MIT"],
-      links: %{"GitHub" => "https://github.com/timbuchwaldt/exmqttc"},
+      links: %{"GitHub" => "https://github.com/timbuchwaldt/exmqttc"}
     }
   end
 
@@ -35,7 +37,7 @@ defmodule Exmqttc.Mixfile do
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:dialyxir, "~> 0.5", only: :dev, runtime: false},
       {:credo, "~> 0.8.0"},
-      {:inch_ex, "~> 0.5", only: :dev},
+      {:inch_ex, "~> 0.5", only: :dev}
     ]
   end
 end
