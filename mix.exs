a91f623d483a0c5119d2022071d5fe68d544cb69
Tim Buchwaldt <tim@buchwaldt.ws>
2018-11-02 21:55:42 +0100

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Exmqttc.Mixfile do
   def project do
     [
       app: :exmqttc,
-      version: "0.6.0",
-      elixir: "~> 1.4",
+      version: "0.6.1",
+      elixir: "~> 1.7",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -33,11 +33,11 @@ defmodule Exmqttc.Mixfile do
   defp deps do
     [
       {:emqttc, github: "emqtt/emqttc", only: [:dev, :test]},
-      {:uuid, "~> 1.1"},
+      {:elixir_uuid, "~> 1.2"},
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:dialyxir, "~> 0.5", only: :dev, runtime: false},
-      {:credo, "~> 0.9", only: [:dev, :test], runtime: false},
-      {:inch_ex, "~> 0.5", only: :dev}
+      {:credo, "~> 0.10", only: [:dev, :test], runtime: false},
+      {:inch_ex, "~> 1.0", only: :dev}
     ]
   end
 end
